fe269ee6824b74e0cd296192e21286b55134fbdb
rmzse <rodrigo@therawdelicious.com>
2017-01-17 18:10:02 +0100

    add results card to bmi tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,14 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('BmiController', function() {
-
+.controller('BmiController', function($scope) {
+  $scope.data = {};
+  $scope.calculateBMI = function() {
+    var person = new Person({
+      weight: $scope.data.weight,
+      height: $scope.data.height
+    });
+    person.calculate_bmi_met();
+    $scope.person = person;
+  };
 });
