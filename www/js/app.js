931ae4afe249566aae3f095be3360fec556e523a
rmzse <rodrigo@therawdelicious.com>
2017-01-17 17:51:17 +0100

    add bmi tab content, route, controller and view, tabs in app working

--- www/js/app.js
+++ www/js/app.js
@@ -47,6 +47,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'AboutController'
         }
       }
+    })
+
+    .state('tab.bmi', {
+      url: '/bmi',
+      views: {
+        'tab-bmi': {
+          templateUrl: 'templates/calculator/calculator.html',
+          controller: 'BmiController'
+        }
+      }
     });
 
   // if none of the above states are matched, use this as the fallback
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,4 +2,8 @@ angular.module('starter.controllers', [])
 
 .controller('AboutController', function(){
 
+})
+
+.controller('BmiController', function() {
+
 });
