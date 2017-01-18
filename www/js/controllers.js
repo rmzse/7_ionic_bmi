f13be518d1dcb7ff6344555a62a3415416c09890
rmzse <rodrigo@therawdelicious.com>
2017-01-18 11:23:01 +0100

    add imperial-metric conversion of values on toggle, not working yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,7 +7,7 @@ angular.module('starter.controllers', [])
   .controller('BmiController', function($scope) {
     $scope.data = {};
 
-    $scope.data.toggle = false;
+    $scope.data.toggle = true;
 
 
     $scope.$on("$ionicView.loaded", function(){ // onLoad of the view, initiate the placeholder values
@@ -16,7 +16,8 @@ angular.module('starter.controllers', [])
 
     $scope.updateToggleValues = function (){ // on switch toggle, replace measurement units
       $scope.setMeasurementUnit();
-      if ( ( $scope.data.weight !== undefined ) || ( $scope.data.height !== undefined ) || ( $scope.data.heightft !== undefined ) || ( $scope.data.heightin !== undefined ) ) { // re-calculate the bmi when toggle is switched unless it's a view init
+      if ( ( ( $scope.data.weight !== undefined ) && ( $scope.data.height !== undefined ) ) || ( ( $scope.data.heightft !== undefined ) && ( $scope.data.heightin !== undefined ) ) ) { // re-calculate the bmi when toggle is switched unless it's a view init
+        $scope.convert();
         $scope.calculateBMI();
       }
     };
@@ -47,11 +48,32 @@ angular.module('starter.controllers', [])
 
       $scope.placeholder = {
         weight: "Weight in " + $scope.units.weight,
-        height: "Weight in " + $scope.units.height,
+        height: "Height in " + $scope.units.height,
         weightlb: "Weight in " + $scope.units.weightlb,
         heightft: "Height in " + $scope.units.heightft,
         heightin: "Height in " + $scope.units.heightin
       };
+    };
+
+    $scope.convert = function () {
+      if ($scope.data.weight == undefined) {
+        $scope.data.weight = parseInt($scope.data.weightlb * 0.45359237);
+      }
+
+      // if ( ( ( $scope.data.weight == undefined ) && ( $scope.data.height == undefined ) ) {
+      //   $scope.data.weight = parseInt($scope.data.weightlb * 0.45359237);
+      //   $scope.data.height = parseInt( ($scope.data.heightft * 12 + $scope.data.heightin) * 2.54 );
+      // }
+
+      // if ( ( ( $scope.data.weight !== undefined ) && ( $scope.data.height !== undefined ) ) {
+      //   $scope.data.weight = parseInt($scope.data.weightlb * 0.45359237);
+      //   $scope.data.height = parseInt( ($scope.data.heightft * 12 + $scope.data.heightin) * 2.54 );
+      // }
+      // else if ( ( $scope.data.weightlb !== undefined ) && ( $scope.data.heightft !== undefined ) && ( $scope.data.heightin !== undefined ) ) {
+      //   $scope.data.weightlb = parseInt($scope.data.weight / 0.45359237);
+      //   $scope.data.heightft = parseInt(parseInt(data.height / 2.54) / 12);   // get 5.5ft if 66inches and 5 with parseInt
+      //   $scope.data.heightin = parseInt(parseInt(data.height / 2.54) % 12);   // and then 6 inches with mod 12 and parseInt
+      // }
     };
 
     $scope.calculateBMI = function() {
